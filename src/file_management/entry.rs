21c22ea3d591ed6d21bed715e0cc9cd22c3ea014
Daniel da Silva <dsilvaba@pm.me>
2025-11-23 03:29:55 +0100

    Fixes

--- src/file_management/entry.rs
+++ src/file_management/entry.rs
@@ -92,65 +92,59 @@ impl Directory {
             metadata: entry.metadata()?,
             // TODO: Implement config with serde using file at ~/.config/diode/config.toml
             // Increase to preload more directories
-            entries: Self::read_directory_recursive(&entry.path(), 0)?,
+            entries: Self::recurse(&entry.path(), 1, 0)?,
         })
     }
 
-    fn from_without_entries(entry: &DirEntry) -> io::Result<Self> {
+    fn from_recursive(
+        entry: &DirEntry,
+        max_depth: usize,
+        current_depth: usize,
+    ) -> io::Result<Self> {
         Ok(Self {
             name: entry.file_name(),
             path: entry.path(),
             metadata: entry.metadata()?,
-            entries: Vec::new(),
+            entries: Self::recurse(&entry.path(), max_depth, current_depth)?,
         })
     }
 
-    fn read_directory_recursive(path: &Path, max_depth: usize) -> io::Result<Vec<Box<dyn Entry>>> {
-        fn recurse(
-            path: &Path,
-            max_depth: usize,
-            current_depth: usize,
-            paths: &mut Vec<Box<dyn Entry>>,
-        ) -> io::Result<()> {
-            if current_depth > max_depth {
-                return Ok(());
-            }
+    fn recurse(
+        path: &Path,
+        max_depth: usize,
+        current_depth: usize,
+    ) -> io::Result<Vec<Box<dyn Entry>>> {
+        let mut paths: Vec<Box<dyn Entry>> = Vec::new();
 
-            let read_dir = graceful_return!(fs::read_dir(path));
-
-            for entry in read_dir {
-                let entry = graceful_return!(entry);
-                let file_type = graceful_return!(entry.file_type());
-                match (
-                    file_type.is_file(),
-                    file_type.is_dir(),
-                    file_type.is_symlink(),
-                ) {
-                    (true, _, _) => {
-                        paths.push(Box::new(graceful_return!(File::from(&entry))));
-                    }
-                    (_, true, _) => match current_depth >= max_depth {
-                        true => paths.push(Box::new(graceful_return!(
-                            Directory::from_without_entries(&entry)
-                        ))),
-                        false => paths.push(Box::new(graceful_return!(Directory::from(&entry)))),
-                    },
-                    (_, _, true) => {
-                        paths.push(Box::new(graceful_return!(Symlink::from(&entry))));
-                    }
-                    _ => continue,
-                };
-
-                if path.is_dir() && current_depth < max_depth {
-                    recurse(path, max_depth, current_depth + 1, paths)?;
-                }
-            }
-
-            Ok(())
+        if current_depth > max_depth {
+            return Ok(paths);
         }
 
-        let mut paths = Vec::new();
-        recurse(path, max_depth, 0, &mut paths)?;
+        let read_dir = graceful_return!(fs::read_dir(path), Ok(paths));
+
+        for entry in read_dir {
+            let entry = graceful_return!(entry, Ok(paths));
+            let file_type = graceful_return!(entry.file_type(), Ok(paths));
+
+            match (
+                file_type.is_file(),
+                file_type.is_dir(),
+                file_type.is_symlink(),
+            ) {
+                (true, _, _) => {
+                    paths.push(Box::new(graceful_return!(File::from(&entry), Ok(paths))));
+                }
+                (_, true, _) => paths.push(Box::new(graceful_return!(
+                    Directory::from_recursive(&entry, max_depth, current_depth + 1),
+                    Ok(paths)
+                ))),
+                (_, _, true) => {
+                    paths.push(Box::new(graceful_return!(Symlink::from(&entry), Ok(paths))));
+                }
+                _ => continue,
+            };
+        }
+
         Ok(paths)
     }
 }
